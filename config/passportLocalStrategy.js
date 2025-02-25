36500c703023a5ac8d60f90c6072d4af066a7f23
rakeshkidecha <kidecharakesh2002@gmail.com>
2025-02-25 11:28:55 +0530

    Save Change

--- config/passportLocalStrategy.js
+++ config/passportLocalStrategy.js
@@ -42,7 +42,7 @@ passport.use('userLogin',new LocalStrategy({usernameField:'email',passReqToCallb
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8012/auth/google/callback',
+    callbackURL: 'https://e-commers-lvga.onrender.com/auth/google/callback',
 },async(accessToken, refreshToken, profile, done)=>{
     const isExistUser = await User.findOne({email:profile.emails[0].value});
     if(isExistUser){
